eb834449cfde3a35f6656233fb3de3e7c5280058
Aryan <83343332+ArataCracker@users.noreply.github.com>
2025-07-01 15:38:44 +0530

    added vercel.json

--- frontend/src/components/BlogCard.tsx
+++ frontend/src/components/BlogCard.tsx
@@ -46,7 +46,7 @@ export function Circle(){
     return <div className="h-1 w-1 rounded-full bg-slate-500 "></div>
 }
 
-export function Avatar({ name, size = "small" }: {name: string, size: "small"|"big"}) {
+export function Avatar({ name, size = "small" }: {name: string, size?: "small"|"big"}) {
     return <div className={`relative inline-flex items-center justify-center  overflow-hidden bg-gray-100 rounded-full dark:bg-gray-600 ${ size === "small"? "w-6 h-6": "w-10 h-10"}`}>
     <span className={`${size === "small"? "text-xs": "text-md"} text-gray-600 dark:text-gray-300`}>
         {name[0]}    
